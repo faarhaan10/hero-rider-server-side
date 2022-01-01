000f1cdf615a043c808dbdbb2e0169c0552599d2
Farhan Elias <faarhaan10@gmail.com>
2022-01-01 20:39:00 +0600

    users api done

--- index.js
+++ index.js
@@ -10,6 +10,44 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+//mongodb connection tools
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.y4qnm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+async function run() {
+    try {
+        await client.connect();
+
+        //db
+        const database = client.db("heroRider");
+        // db collections
+        const userCollection = database.collection("users");
+
+        //post users
+        app.post('/users', async (req, res) => {
+            const doc = req.body;
+            const result = await userCollection.insertOne(doc);
+            res.send(result);
+        });
+        //get users
+        app.get('/users', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            const user = await userCollection.findOne(query);
+            let isAdmin = false;
+            if (user?.role === 'admin') {
+                isAdmin = true;
+            }
+            res.json({ admin: isAdmin });
+        });
+    }
+    finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
 
 
 //default api's
